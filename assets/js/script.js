c70e0a3abdd8ce0d597dd080e5cd0b5f9ed7b389
trm621 <t.maher621@gmail.com>
2021-10-26 13:09:06 -0400

    changed some file locations

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,9 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var currentDay = moment().format("MMMM Do, YYYY");
+
+console.log("currendDay");
+
 $(".row").append(".container");
 
