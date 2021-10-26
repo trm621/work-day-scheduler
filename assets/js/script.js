4f585325cca92671426bafd3dae7902ffb28d105
trm621 <t.maher621@gmail.com>
2021-10-26 18:03:56 -0400

    added ability to push items to an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
-var tasks = {};
+var tasks = [];
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
 $(".container #textContainer").on("click", function() {
     var text = $(this).text();
     var textInput = $("<textarea>")
-    .addClass("form-control col-10 text-light bg-secondary mb-2 p-5")
+    .addClass("form-control col-10 text-light bg-secondary mb-2 p-4")
     .val(text);
 
     $(this).replaceWith(textInput);
+    tasks.push({
+        text: $(textInput)
+    });
 });
+
+$(".container #save-button").on("click", function() {
+    saveTasks();
+});
+
+loadTasks();
