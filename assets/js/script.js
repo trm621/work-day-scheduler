f72ede55c4d0d7b65d2a4287d70853ce76b77b53
trm621 <t.maher621@gmail.com>
2021-10-26 17:13:15 -0400

    added ability to input tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,10 @@ var saveTasks = function() {
 };
 
 $(".container #textContainer").on("click", function() {
-    console.log("this was clicked!");
+    var text = $(this).text();
+    var textInput = $("<textarea>")
+    .addClass("form-control col-10 text-light bg-secondary mb-2 p-5")
+    .val(text);
+
+    $(this).replaceWith(textInput);
 });
