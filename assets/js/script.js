0862ac32a8055d70ed3fa5c312551435d55a9476
trm621 <t.maher621@gmail.com>
2021-10-28 15:43:01 -0400

    task persistence works now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,33 @@
-var tempArr = [];
-
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tempArr));
-};
-
 var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    for (var i = 0; i < tasks.length; i++)
-    if (tasks[i].time = eightAm) {
-        $("#eightAm").siblings("textarea").val(tasks[i].text)
-    };
+    eightOclock = localStorage.getItem("eightAm")
+    $("#eightAm").siblings("textArea").val(eightOclock);
+
+    nineOclock = localStorage.getItem("nineAm")
+    $("#nineAm").siblings("textArea").val(nineOclock);
+
+    tenOclock = localStorage.getItem("tenAm")
+    $("#tenAm").siblings("textArea").val(tenOclock);
+
+    elevenOclock = localStorage.getItem("elevenAm")
+    $("#elevenAm").siblings("textArea").val(elevenOclock);
+
+    twelveOclock = localStorage.getItem("twelvePm")
+    $("#twelvePm").siblings("textArea").val(twelveOclock);
+
+    oneOclock = localStorage.getItem("onePm")
+    $("#onePm").siblings("textArea").val(oneOclock);
+
+    twoOclock = localStorage.getItem("twoPm")
+    $("#twoPm").siblings("textArea").val(twoOclock);
+
+    threeOclock = localStorage.getItem("threePm")
+    $("#threePm").siblings("textArea").val(threeOclock);
+    
+    fourOclock = localStorage.getItem("fourPm")
+    $("#fourPm").siblings("textArea").val(fourOclock);
+
+    fiveOclock = localStorage.getItem("fivePm")
+    $("#fivePm").siblings("textArea").val(fiveOclock);
 };
 
 var auditTask = function(tasks) {
@@ -18,11 +36,7 @@ var auditTask = function(tasks) {
 $(".save-button").on("click", function() {
     var textAreaVal = $(this).siblings("textarea").val().trim();
     var textAreaId = $(this).attr("id").trim();
-   tempArr.push({
-       text: textAreaVal,
-       time: textAreaId
-   });
-   saveTasks(textAreaVal, textAreaId)
+    localStorage.setItem("" + textAreaId, textAreaVal);
 });
 
 loadTasks();
