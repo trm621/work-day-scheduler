a5b665a9105e88dc97a83999f114e26682462fa3
trm621 <t.maher621@gmail.com>
2021-10-28 14:10:51 -0400

    made it so loadTasks converts localStorage into an array of objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
-var tasks = [];
+var tempArr = [];
 
 var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tempArr));
 };
 
 var loadTasks = function() {
-    localStorage.getItem("tasks");
+    tasks = JSON.parse(localStorage.getItem("tasks"));
     console.log(tasks);
+    for (var i = 0; i < tasks.length; i++)
+    if (tasks[i].time === eightAm) {
+        textArea.append(eightAm)
+    }
 };
 
-var auditTask = function(taskObj) {
-
+var auditTask = function(tasks) {
 }
 
 $(".save-button").on("click", function() {
-    var textAreaVal = $(this).siblings("textarea").val();
-    var textAreaId = $(this).attr("id");
-   tasks.push({
+    var textAreaVal = $(this).siblings("textarea").val().trim();
+    var textAreaId = $(this).attr("id").trim();
+   tempArr.push({
        text: textAreaVal,
        time: textAreaId
    });
    saveTasks(textAreaVal, textAreaId)
 });
-
-
 
 loadTasks();
